c421a2fb643e37ce9d1158a13883ec634994ec4a
dasscheman <d.asscheman@lumc.nl>
2015-07-01 17:43:25 +0200

    2015/07/01-17:43:23

--- trunk/src/individuals.php
+++ trunk/src/individuals.php
@@ -207,13 +207,15 @@ if (PATH_COUNT == 1 && ACTION == 'create') {
 
             $nID = $_DATA->insertEntry($_POST, $aFields);
 
-            // Get genes which are modified only when phenotype is puplic
+            // Get genes which are modified only when individual and variant status is marked or puplic.
             if ($_POST['statusid']>=STATUS_MARKED){
                 $aGenes = $_DB->query('SELECT DISTINCT t.geneid FROM ' . TABLE_TRANSCRIPTS . ' AS t ' .
-                                                      'INNER JOIN ' . TABLE_VARIANTS_ON_TRANSCRIPTS . ' AS vot ON (vot.transcriptid = t.id) ' .
-                                                      'INNER JOIN ' . TABLE_SCR2VAR . ' AS s2v ON (vot.id = s2v.variantid) ' .
-                                                      'INNER JOIN ' . TABLE_SCREENINGS . ' AS s ON (s.id = s2v.screeningid) ' .
-                                                      'WHERE s.individualid = ?', array($nID))->fetchAllColumn();
+                                    'INNER JOIN ' . TABLE_VARIANTS_ON_TRANSCRIPTS . ' AS vot ON (vot.transcriptid = t.id) ' .
+                                    'INNER JOIN ' . TABLE_VARIANTS . ' AS vog ON (vog.id = vot.id) ' .
+                                    'INNER JOIN ' . TABLE_SCR2VAR . ' AS s2v ON (s2v.variantid = vog.id) ' .
+                                    'INNER JOIN ' . TABLE_SCREENINGS . ' AS s ON (s.id = s2v.screeningid) ' .
+                                    'WHERE vog.statusid >= ? ' .
+                                    'AND s.individualid = ?', array(STATUS_MARKED, $nID))->fetchAllColumn();
                 if ($aGenes) {
                     $aGenes = array_unique($aGenes);
                     // Change updated date for genes
@@ -368,27 +370,15 @@ if (PATH_COUNT == 2 && ctype_digit($_PE[1]) && in_array(ACTION, array('edit', 'p
             // FIXME: implement versioning in updateEntry!
             $_DATA->updateEntry($nID, $_POST, $aFields);
 
-            // Get genes which are modified only when phenotype is puplic
-            if ($_POST['statusid']>=STATUS_MARKED){
+            // Get genes which are modified only when individual and variant status is marked or puplic.
+            if ($zData['statusid']>=STATUS_MARKED || $_POST['statusid']>=STATUS_MARKED){
                 $aGenes = $_DB->query('SELECT DISTINCT t.geneid FROM ' . TABLE_TRANSCRIPTS . ' AS t ' .
-                                                      'INNER JOIN ' . TABLE_VARIANTS_ON_TRANSCRIPTS . ' AS vot ON (vot.transcriptid = t.id) ' .
-                                                      'INNER JOIN ' . TABLE_SCR2VAR . ' AS s2v ON (vot.id = s2v.variantid) ' .
-                                                      'INNER JOIN ' . TABLE_SCREENINGS . ' AS s ON (s.id = s2v.screeningid) ' .
-                                                      'WHERE s.individualid = ?', array($nID))->fetchAllColumn();
-                if ($aGenes) {
-                    $aGenes = array_unique($aGenes);
-                    // Change updated date for genes
-                    lovd_setUpdatedDate($aGenes);
-                }
-            }
-            
-            // Get genes which are modified only when phenotype is puplic
-            if ($_POST['statusid']>=STATUS_MARKED){
-                $aGenes = $_DB->query('SELECT DISTINCT t.geneid FROM ' . TABLE_TRANSCRIPTS . ' AS t ' .
-                                                      'INNER JOIN ' . TABLE_VARIANTS_ON_TRANSCRIPTS . ' AS vot ON (vot.transcriptid = t.id) ' .
-                                                      'INNER JOIN ' . TABLE_SCR2VAR . ' AS s2v ON (vot.id = s2v.variantid) ' .
-                                                      'INNER JOIN ' . TABLE_SCREENINGS . ' AS s ON (s.id = s2v.screeningid) ' .
-                                                      'WHERE s.individualid = ?', array($nID))->fetchAllColumn();
+                                    'INNER JOIN ' . TABLE_VARIANTS_ON_TRANSCRIPTS . ' AS vot ON (vot.transcriptid = t.id) ' .
+                                    'INNER JOIN ' . TABLE_VARIANTS . ' AS vog ON (vog.id = vot.id) ' .
+                                    'INNER JOIN ' . TABLE_SCR2VAR . ' AS s2v ON (s2v.variantid = vog.id) ' .
+                                    'INNER JOIN ' . TABLE_SCREENINGS . ' AS s ON (s.id = s2v.screeningid) ' .
+                                    'WHERE vog.statusid >= ? ' .
+                                    'AND s.individualid = ?', array(STATUS_MARKED, $nID))->fetchAllColumn();
                 if ($aGenes) {
                     $aGenes = array_unique($aGenes);
                     // Change updated date for genes
@@ -551,7 +541,24 @@ if (PATH_COUNT == 2 && ctype_digit($_PE[1]) && ACTION == 'delete') {
                 }
             }
 
+            // Get genes which are modified only when individual and variant status is marked or puplic.
+            if ($_POST['statusid']>=STATUS_MARKED){
+                $aGenes = $_DB->query('SELECT DISTINCT t.geneid FROM ' . TABLE_TRANSCRIPTS . ' AS t ' .
+                                    'INNER JOIN ' . TABLE_VARIANTS_ON_TRANSCRIPTS . ' AS vot ON (vot.transcriptid = t.id) ' .
+                                    'INNER JOIN ' . TABLE_VARIANTS . ' AS vog ON (vog.id = vot.id) ' .
+                                    'INNER JOIN ' . TABLE_SCR2VAR . ' AS s2v ON (s2v.variantid = vog.id) ' .
+                                    'INNER JOIN ' . TABLE_SCREENINGS . ' AS s ON (s.id = s2v.screeningid) ' .
+                                    'WHERE vog.statusid >= ? ' .
+                                    'AND s.individualid = ?', array(STATUS_MARKED, $nID))->fetchAllColumn();
+            }
+
             $_DATA->deleteEntry($nID);
+
+            if ($_POST['statusid']>=STATUS_MARKED && $aGenes) {
+                $aGenes = array_unique($aGenes);
+                // Change updated date for genes
+                lovd_setUpdatedDate($aGenes);
+            }
 
             $_DB->commit();
 
