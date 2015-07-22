7e64320840a6d67c23387170f4c4b5a75bf32b8c
dasscheman <d.asscheman@lumc.nl>
2015-07-22 15:51:37 +0200

    2015/07/22-15:51:36

--- trunk/tests/travis/setup.sh
+++ trunk/tests/travis/setup.sh
@@ -15,16 +15,21 @@ if [ ! -d vendor ] || [ ! -f vendor/autoload.php ]; then
     composer install --dev
 fi
 
+echo "Installing nano"
+sudo apt-get install nano
+
 echo "Installing Xvfb"
 sudo apt-get install xvfb
 Xvfb :99 -ac -screen 0 1280x1024x24 export DISPLAY=:99
 
 echo "Installing supervisord"
 sudo apt-get install supervisor -y --no-install-recommends
-sudo cp ./trunk/tests/travis/phpunit-environment.conf /etc/supervisor/conf.d/
-sudo sed -i "s/^directory=.*webserver$/directory=${ESCAPED_BUILD_DIR}\\/selenium-1-tests/" /etc/supervisor/conf.d/phpunit-environment.conf
-sudo sed -i "s/^autostart=.*selenium$/autostart=true/" /etc/supervisor/conf.d/phpunit-environment.conf
-sudo sed -i "s/^autostart=.*python-webserver$/autostart=true/" /etc/supervisor/conf.d/phpunit-environment.conf
+##sudo cp ./trunk/tests/travis/phpunit-environment.conf /etc/supervisor/conf.d/
+##sudo sed -i "s/^directory=.*webserver$/directory=${ESCAPED_BUILD_DIR}\\/selenium-1-tests/" /etc/supervisor/conf.d/phpunit-environment.conf
+##sudo sed -i "s/^autostart=.*selenium$/autostart=true/" /etc/supervisor/conf.d/phpunit-environment.conf
+##sudo sed -i "s/^autostart=.*python-webserver$/autostart=true/" /etc/supervisor/conf.d/phpunit-environment.conf
+
+gnome-terminal -e "java -jar /usr/share/selenium/selenium-server-standalone.jar -trustAllSSLCertificates" & sleep 2s
 
 sudo sed -i -e "s,/var/www,/home/travis/build/dasscheman/LOVD3_development,g" /etc/apache2/sites-available/default
 sudo sed -i -e "s,AllowOverride[ ]None,AllowOverride All,g" /etc/apache2/sites-available/default
