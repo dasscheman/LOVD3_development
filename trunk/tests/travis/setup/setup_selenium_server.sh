b14e93460cac0590ee95faa6c5392e9b4d848570
dasscheman <d.asscheman@lumc.nl>
2015-07-23 10:24:58 +0200

    2015/07/23-10:24:57

--- trunk/tests/travis/setup/setup_selenium_server.sh
+++ trunk/tests/travis/setup/setup_selenium_server.sh
@@ -1,6 +1,6 @@
 serverUrl='http://127.0.0.1:4444'
 serverFile=2.44/selenium-server-standalone-2.44.0.jar
-firefoxUrl=http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/21.0/linux-x86_64/en-US/firefox-21.0.tar.bz2
+firefoxUrl=http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/37.0.2/linux-x86_64/en-US/firefox-37.0.2.tar.bz2
 firefoxFile=firefox.tar.bz2
 phpVersion=`php -v`
 
