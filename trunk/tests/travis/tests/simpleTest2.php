8feb82c320e5998c43663b96c330e2229b43ad6c
dasscheman <d.asscheman@lumc.nl>
2015-07-23 12:04:46 +0200

    2015/07/23-12:04:44

---
+++ trunk/tests/travis/tests/simpleTest2.php
@@ -0,0 +1,19 @@
+<?php
+//require_once 'PHPUnit/Extensions/SeleniumTestCase.php';
+
+class admin_tests extends PHPUnit_Extensions_SeleniumTestCase
+{
+    protected function setUp()
+    {
+        $this->setBrowser('*firefox');
+        $this->setBrowserUrl('http://127.0.0.1/');
+    }
+
+    public function testLoadPage()
+    {
+        $this->open('http://127.0.0.1/trunk/tests/travis/simpleTest.html');
+        $this->waitForPageToLoad ( "30000" );
+        $this->assertTitle('phpunit selenium test');
+    }
+}
+?>
