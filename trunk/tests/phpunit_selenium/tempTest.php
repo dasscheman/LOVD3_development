0076f9e48e4f34c4f9712e3bef7292322d56a6a2
dasscheman <d.asscheman@lumc.nl>
2015-07-23 15:41:03 +0200

    2015/07/23-15:41:02

--- trunk/tests/phpunit_selenium/tempTest.php
+++ trunk/tests/phpunit_selenium/tempTest.php
@@ -114,11 +114,11 @@ class temp_tests extends PHPUnit_Extensions_SeleniumTestCase
         $this->assertTrue((bool)preg_match('/^[\s\S]*\/trunk\/src\/install\/[\s\S]step=3&sent=true$/',$this->getLocation()));
         $this->click("css=input[type=\"submit\"]");
         $this->waitForPageToLoad("30000");
-        $this->assertEquals('trunk/src/install', $this->getLocation());
-        $this->assertTrue((bool)preg_match('/^[\s\S]*\/trunk\/src\/install\/[\s\S]step=4$/',$this->getLocation()));
+        //$this->assertTrue((bool)preg_match('/^[\s\S]*\/trunk\/src\/install\/[\s\S]step=4$/',$this->getLocation()));
         $this->click("css=button");
         $this->waitForPageToLoad("30000");
         $this->assertTrue((bool)preg_match('/^[\s\S]*\/trunk\/src\/setup[\s\S]newly_installed$/',$this->getLocation()));
+        $this->assertEquals('trunk/src/install', $this->getLocation());
     }/*
     public function testCreateGeneIVD()
     {
