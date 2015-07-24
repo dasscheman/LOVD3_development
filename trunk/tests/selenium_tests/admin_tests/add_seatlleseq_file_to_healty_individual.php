49dba13da1addb1c626135ffc04778e16aa9207c
dasscheman <d.asscheman@lumc.nl>
2015-07-24 14:56:35 +0200

    2015/07/24-14:56:34

--- trunk/tests/selenium_tests/admin_tests/add_seatlleseq_file_to_healty_individual.php
+++ trunk/tests/selenium_tests/admin_tests/add_seatlleseq_file_to_healty_individual.php
@@ -26,8 +26,8 @@ class Example extends PHPUnit_Extensions_SeleniumTestCase
     $this->select("name=owned_by", "label=LOVD3 Admin");
     $this->select("name=statusid", "label=Public");
     $this->click("css=input[type=\"submit\"]");
-    // Importing seatlleseq can take some time, therefore the timeout is extended. And seet back to 3000 after seatlleseq is successfully imported.
-    sleep(60);
+    // Importing seatlleseq can take some time, therefore the pause for 120 seconds.
+    $this->setTimeout(300000)
     for ($second = 0; ; $second++) {
         if ($second >= 60) $this->fail("timeout");
         try {
