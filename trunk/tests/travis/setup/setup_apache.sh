cadc427daa191ab359a6a7b6635072f496c27962
dasscheman <d.asscheman@lumc.nl>
2015-07-24 10:02:54 +0200

    2015/07/24-10:02:53

--- trunk/tests/travis/setup/setup_apache.sh
+++ trunk/tests/travis/setup/setup_apache.sh
@@ -4,7 +4,9 @@ sudo apt-get install -y --force-yes apache2 libapache2-mod-php5 php5-curl php5-i
 sudo a2enmod rewrite
 
 ## Setting the home directory for localhost.
-sudo sed -i -e "s,/var/www,/home/travis/build/dasscheman/LOVD3_development,g" /etc/apache2/sites-available/default
+sudo mv /home/travis/build/dasscheman/LOVD3_development /home/travis/build/dasscheman/svn/LOVD3_development
+sudo sed -i -e "s,/var/www,/home/travis/build/dasscheman/svn/LOVD3_development,g" /etc/apache2/sites-available/default
+##sudo sed -i -e "s,/var/www,/home/travis/build/dasscheman/LOVD3_development,g" /etc/apache2/sites-available/default
 sudo sed -i -e "s,AllowOverride[ ]None,AllowOverride All,g" /etc/apache2/sites-available/default
 
 
