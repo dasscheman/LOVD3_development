5f274d41da9ed25e47559379e6fe6b4b2d3b84a1
dasscheman <d.asscheman@lumc.nl>
2015-07-28 13:58:49 +0200

    2015/07/28-13:58:48

--- trunk/tests/phpunit_selenium/adminTest.php
+++ trunk/tests/phpunit_selenium/adminTest.php
@@ -4,7 +4,7 @@
  * LEIDEN OPEN VARIATION DATABASE (LOVD)
  *
  * Created     : 2014-12-19
- * Modified    : 2015-07-28:13:44:05
+ * Modified    : 2015-07-28:13:58:27
  * For LOVD    : 3.0-12
  *
  * Copyright   : 2014 Leiden University Medical Center; http://www.LUMC.nl/
@@ -335,7 +335,7 @@ class admin_tests extends PHPUnit_Extensions_SeleniumTestCase
         $this->assertTrue((bool)preg_match('/^[\s\S]*\/trunk\/src\/submit\/screening\/0000000001$/',$this->getLocation()));
         $this->click("//tr[3]/td[2]/b");
         $this->waitForPageToLoad("30000");
-        $this->waitForPageToLoad("4000");
+        sleep(4);
         $this->assertTrue((bool)preg_match('/^[\s\S]*trunk\/src\/individuals\/00000001$/',$this->getLocation()));
         $this->assertEquals("Public", $this->getText("//tr[8]/td"));
     }
