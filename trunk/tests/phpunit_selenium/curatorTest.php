cf51ee039192e776a39e656c4c88aa4ecbd35177
dasscheman <d.asscheman@lumc.nl>
2015-07-28 10:27:16 +0200

    2015/07/28-10:27:14

--- trunk/tests/phpunit_selenium/curatorTest.php
+++ trunk/tests/phpunit_selenium/curatorTest.php
@@ -4,7 +4,7 @@
  * LEIDEN OPEN VARIATION DATABASE (LOVD)
  *
  * Created     : 2014-12-19
- * Modified    : 2015-07-28:10:23:33
+ * Modified    : 2015-07-28:10:26:57
  * For LOVD    : 3.0-12
  *
  * Copyright   : 2014 Leiden University Medical Center; http://www.LUMC.nl/
--- trunk/tests/selenium_tests/collaborator_tests/finish_individual_diagnosed_with_CMT.php
+++ trunk/tests/selenium_tests/collaborator_tests/finish_individual_diagnosed_with_CMT.php
@@ -13,8 +13,7 @@ class Example extends PHPUnit_Extensions_SeleniumTestCase
     $this->assertTrue((bool)preg_match('/^[\s\S]*\/trunk\/src\/submit\/screening\/0000000002$/',$this->getLocation()));
     $this->click("//tr[3]/td[2]/b");
     $this->waitForPageToLoad("30000");
-    $this->assertTrue((bool)preg_match('/^Successfully processed your submission and sent an email notification to the relevant curator[\s\S]*$/',$this->getText("css=table[class=info]")));
-    $this->waitForPageToLoad("4000");
+    $this->waitForPageToLoad("10000");
     $this->assertTrue((bool)preg_match('/^[\s\S]*\/trunk\/src\/individuals\/00000001$/',$this->getLocation()));
   }
 }
