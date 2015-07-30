ff3048d332ffef4b56b98aeef97423dbd0da6ba3
dasscheman <d.asscheman@lumc.nl>
2015-07-30 11:45:38 +0200

    2015/07/30-11:45:38

--- trunk/tests/selenium_tests/admin_tests/finish_individual_diagnosed_healty.php
+++ trunk/tests/selenium_tests/admin_tests/finish_individual_diagnosed_healty.php
@@ -14,6 +14,7 @@ class Example extends PHPUnit_Extensions_SeleniumTestCase
     $this->waitForPageToLoad("30000");
     sleep(4);
     $this->assertContains("ASDFASDFASDF", $this->getBodyText());
+    $this->assertEquals("Public", $this->getLocation());
     $this->assertTrue((bool)preg_match('/^[\s\S]*trunk\/src\/individuals\/00000001$/',$this->getLocation()));
     $this->assertEquals("Public", $this->getText("//tr[8]/td"));
   }
