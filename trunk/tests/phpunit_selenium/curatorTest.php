b20a2d179306ca7526ae187c034430b34a1a4bc9
dasscheman <d.asscheman@lumc.nl>
2015-07-31 13:09:20 +0200

    2015/07/31-13:09:18

--- trunk/tests/phpunit_selenium/curatorTest.php
+++ trunk/tests/phpunit_selenium/curatorTest.php
@@ -4,7 +4,7 @@
  * LEIDEN OPEN VARIATION DATABASE (LOVD)
  *
  * Created     : 2014-12-19
- * Modified    : 2015-07-31:12:57:52
+ * Modified    : 2015-07-31:13:09:07
  * For LOVD    : 3.0-12
  *
  * Copyright   : 2014 Leiden University Medical Center; http://www.LUMC.nl/
