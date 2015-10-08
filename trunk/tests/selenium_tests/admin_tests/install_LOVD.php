7ce82f9f58465f0e2b9a32947e3633bb1808a114
dasscheman <d.asscheman@lumc.nl>
2015-10-08 08:31:39 +0200

    2015/10/08-08:31:38

--- trunk/tests/selenium_tests/admin_tests/install_LOVD.php
+++ trunk/tests/selenium_tests/admin_tests/install_LOVD.php
@@ -10,7 +10,7 @@ class Example extends PHPUnit_Extensions_SeleniumTestCase
   public function testMyTestCase()
   {
     $this->open("/svn/LOVD3/trunk/src/install/");
-    $this->assertContains("/svn/LOVD3/trunk/src/install/", $this->getLocation());
+    $this->assertContains("/trunk/src/install/", $this->getLocation());
     $this->assertContains("submit", $this->getBodyText);
     $this->click("css=input[type=\"submit\"]");
     $this->waitForPageToLoad("30000");
