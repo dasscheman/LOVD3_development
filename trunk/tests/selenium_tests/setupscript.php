068607f04c828ee3f105106d9243fc6eabfd39b8
dasscheman <d.asscheman@lumc.nl>
2015-12-10 13:52:55 +0100

    travis test

--- trunk/tests/selenium_tests/setupscript.php
+++ trunk/tests/selenium_tests/setupscript.php
@@ -28,8 +28,6 @@
  *
  *************/
 
-include 'vendor/phpunit/phpunit-selenium/PHPUnit/Extensions/SeleniumTestCase.php';
-
 class setupscript extends PHPUnit_Extensions_SeleniumTestCase
 {
     protected $captureScreenshotOnFailure = TRUE;
