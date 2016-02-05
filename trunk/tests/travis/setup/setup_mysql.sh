2025b0c2de52a429aab6e38121b8130fdd2e5002
dasscheman <d.asscheman@lumc.nl>
2016-02-05 15:55:23 +0100

    changed folders.

--- trunk/tests/travis/setup/setup_mysql.sh
+++
@@ -1,5 +0,0 @@
-echo "Setup mysql"
-mysql -e "create database IF NOT EXISTS lovd3_development;" -u root
-mysql -e "CREATE USER 'lovd'@'localhost' IDENTIFIED BY 'lovd_pw';" -u root
-mysql -e "GRANT ALL PRIVILEGES ON * . * TO 'lovd'@'localhost';" -u root
-mysql -e "FLUSH PRIVILEGES;"
